30e3a71b15b703b900cac4f8c882da4af89d96d2
Limaomao821 <1114320079@qq.com>
2019-12-06 00:15:54 +0800

    add arithmetic (multiply not included) in stage ex

--- ex.v
+++ ex.v
@@ -133,6 +133,8 @@ module ex(
     wire ov_sum;
     wire[31:0]  reg2_i_mux;
     wire[31:0]  result_sum;
+    wire        reg1_lt_reg2;
+    wire[31:0]  reg1_i_not;
 
     assign reg2_i_mux = (aluop_i==8'b00100010||
                          aluop_i==8'b00100011||
@@ -144,13 +146,53 @@ module ex(
     assign ov_sum = ((!reg1_i[31]&&!reg2_i[31]&&result_sum[31])||
                     (reg1_i[31]&&reg2_i[31]&&!result_sum[31]));
 
+    assign reg1_lt_reg2 =   (aluop_i==8'b00101010)?
+                            (reg1_i[31]&&!reg2_i[31])||
+                            (!reg1_i[31]&&!reg2_i[31]&&result_sum[31])||
+                            (reg1_i[31]&&reg2_i[31]&&result_sum[31]):
+                            (reg1_i<reg2_i);
+    
+    assign reg1_i_not   = ~reg1_i;
     
     always @(*) begin
         case(aluop_i)
-            8'b00100000: begin  //add
-                
+            8'b00100000, 8'b01010101, 8'b00100001, 8'b01010110: begin  //add, addi, addu, addiu
+                arithmaticres <=    result_sum;
             end
-            default: begin                
+            8'b00100010, 8'b00100011: begin
+                arithmaticres <=    result_sum;
+            end
+            8'b00101010, 8'b00101011: begin
+                arithmaticres <=    reg1_lt_reg2;
+            end
+            8'b10110000: begin
+                arithmeticres <=    reg1_i[31] ? 0 : reg1_i[30] ? 1 : reg1_i[29] ? 2 :
+                                    reg1_i[28] ? 3 : reg1_i[27] ? 4 : reg1_i[26] ? 5 :
+                                    reg1_i[25] ? 6 : reg1_i[24] ? 7 : reg1_i[23] ? 8 : 
+                                    reg1_i[22] ? 9 : reg1_i[21] ? 10 : reg1_i[20] ? 11 :
+                                    reg1_i[19] ? 12 : reg1_i[18] ? 13 : reg1_i[17] ? 14 : 
+                                    reg1_i[16] ? 15 : reg1_i[15] ? 16 : reg1_i[14] ? 17 : 
+                                    reg1_i[13] ? 18 : reg1_i[12] ? 19 : reg1_i[11] ? 20 :
+                                    reg1_i[10] ? 21 : reg1_i[9] ? 22 : reg1_i[8] ? 23 : 
+                                    reg1_i[7] ? 24 : reg1_i[6] ? 25 : reg1_i[5] ? 26 : 
+                                    reg1_i[4] ? 27 : reg1_i[3] ? 28 : reg1_i[2] ? 29 : 
+                                    reg1_i[1] ? 30 : reg1_i[0] ? 31 : 32 ;
+            end
+            8'b10110001: begin
+                arithmeticres <=    (reg1_i_not[31] ? 0 : reg1_i_not[30] ? 1 : reg1_i_not[29] ? 2 :
+                                    reg1_i_not[28] ? 3 : reg1_i_not[27] ? 4 : reg1_i_not[26] ? 5 :
+                                    reg1_i_not[25] ? 6 : reg1_i_not[24] ? 7 : reg1_i_not[23] ? 8 : 
+                                    reg1_i_not[22] ? 9 : reg1_i_not[21] ? 10 : reg1_i_not[20] ? 11 :
+                                    reg1_i_not[19] ? 12 : reg1_i_not[18] ? 13 : reg1_i_not[17] ? 14 : 
+                                    reg1_i_not[16] ? 15 : reg1_i_not[15] ? 16 : reg1_i_not[14] ? 17 : 
+                                    reg1_i_not[13] ? 18 : reg1_i_not[12] ? 19 : reg1_i_not[11] ? 20 :
+                                    reg1_i_not[10] ? 21 : reg1_i_not[9] ? 22 : reg1_i_not[8] ? 23 : 
+                                    reg1_i_not[7] ? 24 : reg1_i_not[6] ? 25 : reg1_i_not[5] ? 26 : 
+                                    reg1_i_not[4] ? 27 : reg1_i_not[3] ? 28 : reg1_i_not[2] ? 29 : 
+                                    reg1_i_not[1] ? 30 : reg1_i_not[0] ? 31 : 32) ;
+            end
+            default: begin
+               arithmaticres <=     32'h00000000; 
             end
         endcase
     end
