402dc6e0145310c6ab563d871adc9da1e4a79188
limaomao <1114320079@qq.com>
2020-03-10 20:20:01 +0800

    test

--- if_id.v
+++ if_id.v
@@ -1,9 +1,10 @@
 module if_id(
-    input wire[31:0] if_pc,
-    input wire[31:0] if_inst,
+    input wire[31:0]    if_pc,
+    input wire[31:0]    if_inst,
+    input wire[5:0]     stall,
 
-    input wire       rst,
-    input wire       clk,
+    input wire          rst,
+    input wire          clk,
 
     output reg[31:0] id_pc,
     output reg[31:0] id_inst
@@ -12,9 +13,11 @@ module if_id(
         if(rst == 1'b1) begin
             id_pc <= 32'h00000000;
             id_inst <= 32'h00000000;
-        end else begin
-            id_pc <= if_pc;
+        end else if(stall[1] == `Stop && stall[2] == `NoStop) begin
+            id_pc <= `ZeroWord;
+            id_inst <= `ZeroWord;
+        end else if(stall[1] == 'NoStop) begin
+            id_pc   <= if_pc;
             id_inst <= if_inst;
-        end
     end
 endmodule
--- pc_reg.v
+++ pc_reg.v
@@ -1,6 +1,7 @@
 module pc_reg(
     input wire          rst,
     input wire          clk,
+    input wire[5:0]     stall,
 
     output reg[31:0]    pc,
     output reg          ce
@@ -27,8 +28,10 @@ module pc_reg(
     always @ (posedge clk) begin
         if(rst == 1'b1) begin
             pc <= 32'h00000000;
-        end else if(ce == 1'b1) begin
-            pc <= pc + 4;
+        end else if(stall[0] == `NoStop) begin
+            pc <= pc + 4'h4;
+        end else begin
+            pc <= pc;
         end
     end
 endmodule
